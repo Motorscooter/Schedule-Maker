b6d4881bb78c4f56812031d78d535989d268bc28
Motorscooter <Sdownard88@gmail.com>
2020-01-20 15:46:05 -0700

    Updated schedule_maker method
    Added more lines to the method to complete it. Need to fix while loop for correct math.

--- Schedule_Maker.py
+++ Schedule_Maker.py
@@ -33,15 +33,26 @@ def schedule_creator(year=0, month=0, day=0, num_of_days=0):
         start_date = datetime.datetime.now()
     else:
         start_date = datetime.datetime(year, month, day)
-    day_delta = datetime.timedelta(days=number_of_days)
+    day_delta = datetime.timedelta(days=num_of_days)
     end_date = start_date + day_delta
     iter_date = start_date
-    start_col = 1
+    year_start_col = 1
+    month_start_col = 1
     end_col = 1
     workbook = Workbook(str(start_date.year)+str(start_date.month) + str(start_date.day) + '.xlsx')
     worksheet = workbook.add_worksheet('Schedule')
-    cell_format = workbook.add_format({'bold': True, 'valign': True})
-    while end_date - iter_date <= 0:
+    cell_format = workbook.add_format()
+    cell_format.set_border(1)
+    cell_format.set_bold()
+    cell_format.set_align('center')
+    cell_format.set_align('vcenter')
+    month_format = workbook.add_format()
+    month_format.set_left(2)
+    month_format.set_right(2)
+    year_format = workbook.add_format()
+    year_format.set_left(5)
+    year_format.set_right(5)
+    while datetime.timedelta(days = end_date - iter_date) <= 0:
         while iter_date.month <= 12:
             while iter_date.day <= max(calendar.monthrange(iter_date.year, iter_date.month)):
                 worksheet.write(2, end_col, day_list[calendar.weekday(iter_date.year,iter_date.month, iter_date.day)],
@@ -49,4 +60,9 @@ def schedule_creator(year=0, month=0, day=0, num_of_days=0):
                 worksheet.write(3, end_col, iter_date.day, cell_format)
                 iter_date = iter_date + datetime.timedelta(days=1)
                 end_col += 1
-            worksheet.merge_range(1, start_col, 1, end_date, calendar.month_name[iter_date.month], cell_format)
+            worksheet.merge_range(1, month_start_col, 1, end_col, calendar.month_name[iter_date.month], cell_format)
+            worksheet.set_column(month_start_col,end_col,month_format)
+            month_start_col = end_col + 1
+        worksheet.merge_range(0,year_start_col,0,end_col, cell_format)
+        worksheet.set_column(year_start_col,end_col,year_format)
+    workbook.close()
